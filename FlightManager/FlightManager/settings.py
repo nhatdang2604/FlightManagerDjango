13cbaaf13971383d484a833c6416d109286bbb2d
dangkl123 <nhatdang2604@gmail.com>
2022-06-06 22:48:20 +0700

    debug on dbs connection

--- FlightManager/FlightManager/settings.py
+++ FlightManager/FlightManager/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
 # }
 
 import dj_database_url
-db_from_env = dj_database_url.config(con_max_age = 600)
+db_from_env = dj_database_url.config(conn_max_age = 600)
 DATABASES['default'].update(db_from_env)
 
 # Password validation
@@ -136,7 +136,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = BASE_DIR/'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = 'static/'
 
