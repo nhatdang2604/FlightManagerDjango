b007467eb513dbb1024806055016bbc9d9fe801e
dangkl123 <nhatdang2604@gmail.com>
2022-06-06 23:21:50 +0700

    create staticfiles folder

--- FlightManager/FlightManager/settings.py
+++ FlightManager/FlightManager/settings.py
@@ -148,8 +148,6 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/upload/images')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
